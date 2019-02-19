4776df974d83a0d65eb5847f59987797318a64be
Asbert Ma <stayinalive@msn.cn>
2019-02-19 11:00:11 +0800

    apply recaptcha key

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import { VueReCaptcha } from 'vue-recaptcha-v3';
 
-Vue.use(VueReCaptcha, { siteKey: '6LerrJEUAAAAAOw6G07D1CZ87gRmrEmXO9m-BYAX'});
+Vue.use(VueReCaptcha, {
+  siteKey: '6LfLo5EUAAAAACZJiD1pMTt0nf2XvFOXK3jTdEhx',
+  loaderOptions: {
+    useRecaptchaNet: true,
+  },
+});
 Vue.config.productionTip = false;
 
 new Vue({
