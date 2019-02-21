009e3d9c24b6735eb03b7760ec80fb69b9c83de5
Asbert Ma <stayinalive@msn.cn>
2019-02-21 17:37:22 +0800

    vue-analytics

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import 'nes.css/css/nes.css';
 import Vue from 'vue';
 import App from './App.vue';
 import { VueReCaptcha } from 'vue-recaptcha-v3';
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueReCaptcha, {
   siteKey: '6LfLo5EUAAAAACZJiD1pMTt0nf2XvFOXK3jTdEhx',
   loaderOptions: {
     useRecaptchaNet: true,
+  },
+});
+Vue.use(VueAnalytics, {
+  id: 'UA-132391998-2',
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production',
   },
 });
 Vue.config.productionTip = false;
