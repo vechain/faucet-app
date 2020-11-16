a58a0a9aa022432915a585828020a9ea19246f1d
Asbert Ma <stayinalive@msn.cn>
2020-11-16 15:51:07 +0800

    connex upgrade & code format

--- src/components/PageFoot.vue
+++ src/components/PageFoot.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="page-foot">
-    <a href="https://github.com/nostalgic-css/NES.css" target="_blank">
-    <i class="nes-icon github"></i>
-    Theme Support by NES.css
-    </a>
-  </div>
+    <div class="page-foot">
+        <a href="https://github.com/nostalgic-css/NES.css" target="_blank">
+            <i class="nes-icon github"></i>
+            Theme Support by NES.css
+        </a>
+    </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class PageFoot extends Vue {}
 </script>
 
 <style>
 .page-foot {
-  position: fixed;
-  left: 20px;
-  bottom: 10px;
+    position: fixed;
+    left: 20px;
+    bottom: 10px;
 }
 </style>
